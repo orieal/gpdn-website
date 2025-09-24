4639917208cd3d49284b9c51051fdf9cba06b904
hexendevelopers <hexenwebcreators@gmail.com>
2025-09-24 21:01:54 +0530

    fix mobile sign in and join us button

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -13,7 +13,7 @@ const MobileMenu = ({ isShowMenu, setIsShowMenu }) => {
         isShowMenu ? "left-0" : "-left-[80vw]"
       } transition-all duration-500 ease-in-out`}
     >
-      <div className="h-full w-full flex flex-col justify-between p-8 pt-20">
+      <div className="h-full w-full flex flex-col justify-start gap-10 p-8 pt-20">
         {/* Navigation Links */}
         <div className="flex flex-col gap-8">
           <Link onClick={() => setIsShowMenu(!isShowMenu)} href={"/"}>
@@ -79,13 +79,13 @@ const MobileMenu = ({ isShowMenu, setIsShowMenu }) => {
         {/* Sign In Button */}
         <div className="pt- border-t border-gray-200 flex flex-col gap-4">
           <Link onClick={() => setIsShowMenu(!isShowMenu)} href="/registration">
-            <button className="w-full bg-black hover:bg-[#039187] cursor-pointer transition-all duration-300 ease-in text-white text-base font-medium py-4 px-6 rounded-xl flex items-center justify-center gap-2 shadow-lg hover:shadow-xl">
+            <button className="w-full bg-black hover:bg-[#039187] cursor-pointer transition-all duration-300 ease-in text-white text-base font-medium py-3 px-3 rounded-xl flex items-center justify-center gap-2 shadow-lg hover:shadow-xl">
               Join Us
               <GoChevronRight className="text-xl" />
             </button>
           </Link>
           <Link onClick={() => setIsShowMenu(!isShowMenu)} href="/signin">
-            <button className="w-full bg-teal-500 hover:bg-[#039187] cursor-pointer transition-all duration-300 ease-in text-white text-base font-medium py-4 px-6 rounded-xl flex items-center justify-center gap-2 shadow-lg hover:shadow-xl">
+            <button className="w-full bg-teal-500 hover:bg-[#039187] cursor-pointer transition-all duration-300 ease-in text-white text-base font-medium py-3 px-3 rounded-xl flex items-center justify-center gap-2 shadow-lg hover:shadow-xl">
               Sign In
               <GoChevronRight className="text-xl" />
             </button>
